522c1feb4db61d965fbe2d13fce6a6d2d9e774ce
Leonardo Mota <93285397+lacarsi@users.noreply.github.com>
2023-09-14 15:22:58 -0300

    Update imc_calculadora.py

--- imc_calculadora.py
+++ imc_calculadora.py
@@ -1,14 +1,49 @@
-altura = input("Insira a sua altura: ")
-peso = input("Insira o seu peso: ")
+import tkinter as tk
+from tkinter import messagebox
 
-peso_as_int = int(peso)
-altura_as_float = float(altura)
+# Função para calcular o IMC
+def calcular_imc():
+    try:
+        altura = float(altura_entry.get())
+        peso = float(peso_entry.get())
+        imc = peso / (altura ** 2)
+        imc_int = int(imc)
+        resultado_label.config(text=f"IMC: {imc_int}")
 
-# Using the exponent operator **
-imc = peso_as_int / altura_as_float ** 2
-# or using multiplication and PEMDAS
-#imc = peso_as_int / (altura_as_float * altura_as_float)
+        # Classificação do IMC
+        if imc < 18.5:
+            classificacao_label.config(text="Abaixo do peso")
+        elif 18.5 <= imc < 24.9:
+            classificacao_label.config(text="Peso normal")
+        elif 25 <= imc < 29.9:
+            classificacao_label.config(text="Sobrepeso")
+        else:
+            classificacao_label.config(text="Obesidade")
+    except ValueError:
+        messagebox.showerror("Erro", "Por favor, insira valores numéricos válidos para altura e peso.")
 
-imc_as_int = int(imc)
+# Configuração da janela
+window = tk.Tk()
+window.title("Calculadora de IMC")
 
-print("IMC: " + str(imc_as_int))
+# Entradas de altura e peso
+altura_label = tk.Label(window, text="Altura (metros):")
+altura_label.pack()
+altura_entry = tk.Entry(window)
+altura_entry.pack()
+
+peso_label = tk.Label(window, text="Peso (quilogramas):")
+peso_label.pack()
+peso_entry = tk.Entry(window)
+peso_entry.pack()
+
+calcular_button = tk.Button(window, text="Calcular IMC", command=calcular_imc)
+calcular_button.pack()
+
+resultado_label = tk.Label(window, text="")
+resultado_label.pack()
+
+classificacao_label = tk.Label(window, text="")
+classificacao_label.pack()
+
+window.mainloop()
